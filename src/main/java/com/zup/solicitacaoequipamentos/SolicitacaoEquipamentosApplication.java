4ebf58dde3231dd9111cf5f91cbac998cd7aedef
carloslopeszup <carlos.lopes@zup.com.br>
2021-05-09 22:28:31 -0300

    IMplementação do end point get e post de usuario

--- src/main/java/com/zup/solicitacaoequipamentos/SolicitacaoEquipamentosApplication.java
+++ src/main/java/com/zup/solicitacaoequipamentos/SolicitacaoEquipamentosApplication.java
@@ -1,13 +1,33 @@
 package com.zup.solicitacaoequipamentos;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SolicitacaoEquipamentosApplication {
+import com.zup.solicitacaoequipamentos.entity.Usuario;
+import com.zup.solicitacaoequipamentos.enums.Role;
+import com.zup.solicitacaoequipamentos.repository.UsuarioRepository;
 
+@SpringBootApplication
+public class SolicitacaoEquipamentosApplication implements CommandLineRunner {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SolicitacaoEquipamentosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Usuario usu = new Usuario(null, "Bruno", "123", "bruno@hotmail.com", Role.USUARIO);
+		Usuario usu2 = new Usuario(null, "Carlos", "123", "carlos@hotmail.com", Role.USUARIO);
+	
+		usuarioRepository.saveAll(Arrays.asList(usu, usu2));
 	}
 
 }
---
+++ src/main/java/com/zup/solicitacaoequipamentos/resource/UsuarioResource.java
@@ -0,0 +1,36 @@
+package com.zup.solicitacaoequipamentos.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zup.solicitacaoequipamentos.entity.Usuario;
+import com.zup.solicitacaoequipamentos.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioResource {
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@PostMapping
+	public ResponseEntity<Usuario>cadastrarUsuario(@RequestBody Usuario usu){
+		
+		return ResponseEntity.ok().body(usuarioService.salvar(usu));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Usuario>>buscarTodosUsuario(){
+		
+		return ResponseEntity.ok().body(usuarioService.buscarTodos());
+	}
+}
