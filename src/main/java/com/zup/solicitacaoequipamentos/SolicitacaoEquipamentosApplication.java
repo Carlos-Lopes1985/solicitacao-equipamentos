e4243aa76b9b5f743f2e61051013d197c801d4ff
carloslopeszup <carlos.lopes@zup.com.br>
2021-05-11 21:54:37 -0300

    retirada dos testes

--- src/main/java/com/zup/solicitacaoequipamentos/SolicitacaoEquipamentosApplication.java
+++ src/main/java/com/zup/solicitacaoequipamentos/SolicitacaoEquipamentosApplication.java
@@ -6,28 +6,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.zup.solicitacaoequipamentos.entity.Usuario;
 import com.zup.solicitacaoequipamentos.enums.Role;
 import com.zup.solicitacaoequipamentos.repository.UsuarioRepository;
 
 @SpringBootApplication
-public class SolicitacaoEquipamentosApplication implements CommandLineRunner {
-
+public class SolicitacaoEquipamentosApplication extends SpringBootServletInitializer implements CommandLineRunner {
+	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(SolicitacaoEquipamentosApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SolicitacaoEquipamentosApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		Usuario usu = new Usuario(null, "Carlos", "123", "carlos@hotmail.com", Role.USUARIO);
+		Usuario usu1= new Usuario(null, "Pedro", "123", "pedro@hotmail.com", Role.USUARIO);
 		
-		Usuario usu = new Usuario(null, "Bruno", "123", "bruno@hotmail.com", Role.USUARIO);
-		Usuario usu2 = new Usuario(null, "Carlos", "123", "carlos@hotmail.com", Role.USUARIO);
-	
-		usuarioRepository.saveAll(Arrays.asList(usu, usu2));
+		usuarioRepository.saveAll(Arrays.asList(usu,usu1));
+		
 	}
 
 }
